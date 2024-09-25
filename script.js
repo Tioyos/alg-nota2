314ef0765c7f5c3663aa88d4e5e1582034e01b10
FELIPE DA SILVA CRUZ BRANDAO <felipe.scbrandao@senacsp.edu.br>
2024-09-25 15:36:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//solicitar a nota do aluno
+
+let nota = parseFloat(prompt("qual foi a nota do aluno"))
+// verificar se o aluno foi aprovado
+
+if ( nota>=7 ){
+   alert("aprovado");
+}else if(nota>=5){
+    alert("recuperacao");
+}else{
+    alert("reprovado")
+                
+
+
+}
+
+
